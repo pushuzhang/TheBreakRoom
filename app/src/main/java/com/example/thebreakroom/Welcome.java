ad2abad063ee1eb9f9ef0b531d2d032234698f77
Pushu Zhang <pzhang32@ucsc.edu>
2021-02-20 10:46:28 -0800

    pull

--- app/src/main/java/com/example/thebreakroom/Welcome.java
+++ app/src/main/java/com/example/thebreakroom/Welcome.java
@@ -1,14 +1,45 @@
 package com.example.thebreakroom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Welcome extends AppCompatActivity {
+
+    private Button welcomeButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mAuth = FirebaseAuth.getInstance();
+        welcomeButton = (Button) findViewById(R.id.welcome_button);
+    }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        mCurrentUser = mAuth.getCurrentUser();
+
+        if(mCurrentUser == null){
+            mAuth.signInAnonymously().addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+
+                }
+                else{}
+            });
+        }
     }
 }
