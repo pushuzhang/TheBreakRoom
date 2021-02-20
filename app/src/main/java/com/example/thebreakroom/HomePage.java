bf1fec9c0c5d08814f94cb60fca31f81a04231ee
Pushu Zhang <pzhang32@ucsc.edu>
2021-02-19 23:44:47 -0800

    bug fix regarding renaming

--- app/src/main/java/com/example/thebreakroom/HomePage.java
+++ app/src/main/java/com/example/thebreakroom/HomePage.java
@@ -2,13 +2,32 @@ package com.example.thebreakroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class HomePage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            Intent startIntent = new Intent(HomePage.this, Welcome.class);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
