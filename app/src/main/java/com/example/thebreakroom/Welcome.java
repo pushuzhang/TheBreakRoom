05d2cdf53f881e5bee8c37b6d7192a53bdecf22a
Ryan <ryskang@ucsc.edu>
2021-02-20 12:17:26 -0800

    Pupu

--- app/src/main/java/com/example/thebreakroom/Welcome.java
+++ app/src/main/java/com/example/thebreakroom/Welcome.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 
 public class Welcome extends AppCompatActivity {
 
+    private Button welcome;
+
+    private FireBaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void onStart() {
+        super.onStart()
     }
 }
