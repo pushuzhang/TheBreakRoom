58b6174527e7353afefc7275fd63e81ddd57fc97
Ryan <ryskang@ucsc.edu>
2021-02-20 12:19:34 -0800

    Pupu

--- app/src/main/java/com/example/thebreakroom/Welcome.java
+++ app/src/main/java/com/example/thebreakroom/Welcome.java
@@ -2,22 +2,52 @@ package com.example.thebreakroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Welcome extends AppCompatActivity {
 
-    private Button welcome;
+    private ImageView mWelcome;
 
-    private FireBaseAuth mAuth;
+    private FirebaseAuth mAuth;
     private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onStart() {
-        super.onStart()
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        mCurrentUser = mAuth.getCurrentUser();
+
+        if(mCurrentUser == null){
+            mAuth.signInAnonymously().addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+
+                    mWelcome = (ImageView) findViewById(R.id.welcome_button);
+
+                    mWelcome.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v){
+                            startActivity(new Intent(Welcome.this, JoinRoom.class));
+                        }
+                    });
+
+                }
+                else{}
+            });
+        }
     }
 }
